4e3858845d0b375546820b9aab369a561878b35f
Fernando Herrera <fernando.herrera85@gmail.com>
2025-04-03 17:19:27 -0400

    feat: integrate TanStack Query for data fetching and add PokedexApp component

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { AppRouter } from './AppRouter.tsx';
 
 import './index.css';
+import { PokedexApp } from './PokedexApp.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppRouter />
+    {/* <AppRouter /> */}
+    <PokedexApp />
   </StrictMode>
 );
