82000c2c62340f2aed006ca2f1a61627d04bb8dd
Fernando Herrera <fernando.herrera85@gmail.com>
2025-04-03 16:47:43 -0400

    feat: enhance RecommendationCard to display move type with color coding

--- src/components/RecommendationCard.tsx
+++ src/components/RecommendationCard.tsx
@@ -2,29 +2,34 @@ import { FC } from 'react';
 import { useNavigate } from 'react-router';
 
 import { BasicPokemon } from '../types/basic-pokemon.interface';
+import { ColorPicker } from '../helpers/color-type';
 
 interface Props {
   pokemon: BasicPokemon;
 }
 
 export const RecommendationCard: FC<Props> = ({ pokemon }) => {
-  const { id, image, name, move } = pokemon;
+  const { id, image, name, move, types } = pokemon;
 
   const navigate = useNavigate();
 
   return (
     <div
       key={id}
-      className="bg-gray-50 rounded-lg p-4 hover:bg-gray-100 transition-colors cursor-pointer"
+      className="flex flex-col justify-center bg-gray-50 rounded-lg p-4 hover:bg-gray-100 transition-colors cursor-pointer"
       onClick={() => navigate(`/pokemons/${id}`)}
     >
       <img src={image} alt={name} className="w-full h-24 object-contain mb-2" />
       <h4 className="font-semibold text-gray-800 capitalize text-center">
         {name}
       </h4>
-      <p className="text-sm text-gray-600 text-center capitalize">
-        {move.replace('-', ' ')}
-      </p>
+      <span
+        className={`${ColorPicker.byType(
+          types[0]
+        )} px-3 py-1 rounded-full text-white text-sm capitalize text-center`}
+      >
+        {move}
+      </span>
     </div>
   );
 };
