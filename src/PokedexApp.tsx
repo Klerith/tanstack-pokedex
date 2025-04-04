7a55fc5c9852aceabcaf79e3a7dbafe259988ca2
Fernando Herrera <fernando.herrera85@gmail.com>
2025-04-04 08:52:54 -0400

    feat: integrate query persistence with TanStack Query and local storage

--- src/PokedexApp.tsx
+++ src/PokedexApp.tsx
@@ -2,6 +2,12 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AppRouter } from './AppRouter';
 
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
+import {
+  persistQueryClient,
+  PersistQueryClientProvider,
+} from '@tanstack/react-query-persist-client';
+
 const queryClient = new QueryClient({
   // TODO: configurar después
   defaultOptions: {
@@ -11,11 +17,25 @@ const queryClient = new QueryClient({
   },
 });
 
+const localStoragePersister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
+
+persistQueryClient({
+  queryClient,
+  persister: localStoragePersister,
+});
+
 export const PokedexApp = () => {
   return (
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister: localStoragePersister }}
+    >
+      {/* <QueryClientProvider client={queryClient}> */}
       <AppRouter />
       <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+      {/* </QueryClientProvider> */}
+    </PersistQueryClientProvider>
   );
 };
