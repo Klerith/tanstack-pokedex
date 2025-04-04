cdefcdca30dcb44c71d4cdaa87950d8b4db087a6
Fernando Herrera <fernando.herrera85@gmail.com>
2025-04-04 09:48:29 -0400

    feat: refactor PokemonPage to use custom hook for data fetching and recommendations

--- src/pages/pokemon-page/PokemonPage.tsx
+++ src/pages/pokemon-page/PokemonPage.tsx
@@ -1,76 +1,22 @@
-import { useEffect } from 'react';
 import { Link, useParams } from 'react-router';
 
 import { RecommendationCard } from '../../components/RecommendationCard';
 import { FullScreenLoading } from '../../components/FullScreenLoading';
 import { PokemonInfo } from './ui/PokemonInfo';
 import { SearchBar } from '../../components/SearchBar';
-import { getPokemonByTerm, getRecommendationAgainst } from '../../actions';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { usePokemon } from '../../hooks/usePokemon';
 
 export const PokemonPage = () => {
   const { nameOrId = '' } = useParams();
-  const queryClient = useQueryClient();
 
   const {
-    data: pokemon,
+    pokemon,
     isError,
     isLoading,
     error,
-  } = useQuery({
-    queryKey: ['pokemon', nameOrId],
-    queryFn: () => getPokemonByTerm(nameOrId),
-    staleTime: 1000 * 60 * 5, // 5 minutos
-  });
-
-  const { data: recommendations = [], isLoading: isLoadingRecommendations } =
-    useQuery({
-      queryKey: ['pokemon', nameOrId, 'recommendations'],
-      queryFn: () => getRecommendationAgainst(pokemon!),
-      enabled: !!pokemon,
-      staleTime: 1000 * 60 * 5, // 5 minutos
-    });
-
-  useEffect(() => {
-    if (pokemon) {
-      queryClient.setQueryData(['pokemon', pokemon.name], pokemon);
-      queryClient.setQueryData(['pokemon', pokemon.id], pokemon);
-    }
-  }, [pokemon]);
-
-  useEffect(() => {
-    if (recommendations.length > 0 && pokemon) {
-      queryClient.setQueryData(
-        ['pokemon', pokemon.name, 'recommendations'],
-        recommendations
-      );
-      queryClient.setQueryData(
-        ['pokemon', pokemon.id, 'recommendations'],
-        recommendations
-      );
-    }
-  }, [recommendations, pokemon]);
-
-  // Prefetch del siguiente y anterior pokemon
-  useEffect(() => {
-    if (pokemon) {
-      const id = `${pokemon.id + 1}`;
-      queryClient.prefetchQuery({
-        queryKey: ['pokemon', id],
-        queryFn: () => getPokemonByTerm(id),
-        staleTime: 1000 * 60 * 5, // 5 minutos
-      });
-    }
-
-    if (pokemon && pokemon.id > 1) {
-      const id = `${pokemon.id - 1}`;
-      queryClient.prefetchQuery({
-        queryKey: ['pokemon', id],
-        queryFn: () => getPokemonByTerm(id),
-        staleTime: 1000 * 60 * 5, // 5 minutos
-      });
-    }
-  }, [pokemon]);
+    recommendations,
+    isLoadingRecommendations,
+  } = usePokemon(nameOrId);
 
   return (
     <div className="min-h-screen  flex items-center justify-center p-4">
